8bc623b9f4fd22b3e474d4dad482811d4f87f0d7
Lesuuh <lesuuh01@gmail.com>
2025-02-15 21:56:56 +0100

    update: expand sample questions with data

--- src/data/sampleQuestions.ts
+++ src/data/sampleQuestions.ts
@@ -1,32 +1,153 @@
 export const questions = [
   {
     id: 1,
-    question: "What is the capital of France?",
-    options: ["London", "Berlin", "Paris", "Madrid"],
-    correctAnswer: 2,
+    question:
+      "What was the main purpose of the pre-validation meeting mentioned in the Explainer 22nd Edition?",
+    options: [
+      "To Discuss ecnomic policies",
+      "To address the learning crisis involving students inability to read, write and comprehend",
+      "To plan a national sports event",
+      "To organize a cultural festival",
+    ],
+    correctAnswer: 1,
   },
   {
     id: 2,
-    question: "Which planet is known as the Red Planet?",
-    options: ["Venus", "Mars", "Jupiter", "Saturn"],
+    question:
+      "According to the Explainer 22nd Edition, what percentage of students transition to tertiary education after finishing primary and secondary education",
+    options: ["50%", "34%", "65%", "25%"],
     correctAnswer: 1,
   },
   {
     id: 3,
-    question: "What is 2 + 2?",
-    options: ["3", "4", "5", "6"],
+    question:
+      "According to the Explainer 25th Edition, which countries have universities whose certificates are recognized in Nigeria",
+    options: [
+      "Ghana and Kenya",
+      "Benin Republic and Togo",
+      "South Africa and Egypt",
+      "United States and United Kingdom",
+    ],
     correctAnswer: 1,
   },
   {
     id: 4,
-    question: "Who painted the Mona Lisa?",
-    options: ["Van Gogh", "Da Vinci", "Picasso", "Rembrandt"],
+    question:
+      "What is the main focus of thte National Education Data System (NEDS) mentioned in the Explainer 25th edition?",
+    options: [
+      "To creat a comprehensive database of all school, students and teachers",
+      "To promote sports in schools",
+      "To organize cultural events",
+      "To provide financial aid to students",
+    ],
     correctAnswer: 1,
   },
   {
     id: 5,
-    question: "What is the chemical symbol for gold?",
-    options: ["Ag", "Fe", "Au", "Cu"],
+    question:
+      "Who was the guest speaker at the seminar mentioned in the Explainer 19th Edition?",
+    options: [
+      "Lanre Issa-Onilu",
+      "Mrs. Rafat Salami",
+      "Prof Tahir Mammam",
+      "Bayo Onanuga",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 6,
+    question:
+      "On Which of this platforms is the mobiliser avaliable for download",
+    options: [
+      "Only on Google Play Store",
+      "Only on Apple Store",
+      "Both playstore and Apple store",
+      "Orange store",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 7,
+    question:
+      "Who is the current Director-General of the NOA who provided an overview of the mobiliser app?",
+    options: [
+      "Garba Abari",
+      "Lanre Issa-Onilu",
+      "Bola Tinubu",
+      "Mohammed Idris",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 8,
+    question:
+      "What is the purpose of the 'Entires' feature in the Mobiliser app",
+    options: [
+      "To submit job applications",
+      "To engage NOA and other government agencies on issues of concern",
+      "To register for events",
+      "To create personal blogs",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 9,
+    question:
+      "What rewards can users earn by participating in the Quiz section of the Mobiliser app?",
+    options: [
+      "Movie tickets",
+      "Recharge cards adn various other prizes",
+      "Concert passes",
+      "Gift vouchers",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 10,
+    question:
+      "What is one of the main objectives of the Mobiliser app according to the Director-General Mallam Lanre Issa-Onilu",
+    options: [
+      "To promote international tourism",
+      "To empower youths to voice thier concerns and stya informed",
+      "To provide financial services",
+      "To offer online shopping discounts",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 11,
+    question:
+      "What solution was propose to address the skills gap among youths, as mentione in the Explainer 22nd Edition?",
+    options: [
+      "Increase in academic subjects",
+      "Exposure to skills",
+      "More sports activites",
+      "Cultural exchange programs",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 12,
+    question:
+      "Who is the Minister of Education mentioned in the Explainer 22nd edition?",
+    options: [
+      "Garba Abari",
+      "Lanre Issa-Onilu",
+      "Prof. Tahir Mamman",
+      "Bayo Onanuga",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 13,
+    question:
+      "What is the theme of the seminar mentioned in the Explainer 19th Edition?",
+    options: [
+      "Understanding the Newsroom",
+      "Economic Development",
+      "Cultural Heritage",
+      "Digital Transformation",
+    ],
     correctAnswer: 2,
   },
 ];
